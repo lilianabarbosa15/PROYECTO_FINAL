2473c2c1a1e434421f19c68f53837a148d043e11
AndreaOspinaHincapie <andreospinah3000@gmail.com>
2020-07-08 20:11:40 -0500

    Implementando métodos de la clase plataforma

--- ProyectoFinal/plataforma.cpp
+++ ProyectoFinal/plataforma.cpp
@@ -1,2 +1,47 @@
 #include "plataforma.h"
 
+double Plataforma::getFrictionC() const
+{
+    return FrictionC;
+}
+
+Plataforma::Plataforma(double _ancho, double _alto, double x_inicial, double y_inicial, double Vx, double Vy,unsigned int max_avance, double friccion)
+{
+    maxAvance=max_avance;
+    velX=Vx;
+    velY=Vy;
+    posX=x_inicial;
+    posY=y_inicial;
+    ancho=_ancho;
+    alto=_alto;
+    FrictionC=friccion;
+    setPos(posX,posY);
+}
+
+void Plataforma::Mover()
+{
+    if(contPos<maxAvance){
+        posX+=velX;
+        posY+=velY;
+        contPos++;
+    }
+    else{
+        velX=-velX;
+        velY=-velY;
+        contPos=0;
+    }
+    setPos(posX,posY);
+
+}
+
+QRectF Plataforma::boundingRect() const
+{
+   return QRectF(posX,posY,ancho,alto);
+
+}
+
+void Plataforma::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->setBrush(Qt::magenta);
+    painter->drawRect(boundingRect());
+}
